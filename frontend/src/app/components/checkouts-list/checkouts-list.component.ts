3cdcf3073da7e72e0506d4d85ceb8a005c90f63f
Kristjanmn <kristjanmn@hotmail.com>
2021-11-29 03:40:30 +0200

    Added filtering and fixed sorting for checkouts-list

--- frontend/src/app/components/checkouts-list/checkouts-list.component.ts
+++ frontend/src/app/components/checkouts-list/checkouts-list.component.ts
@@ -4,6 +4,7 @@ import {MatSort, Sort} from "@angular/material/sort";
 import {MatPaginator} from "@angular/material/paginator";
 import {Checkout} from "../../models/checkout";
 import {CheckoutService} from "../../services/checkout.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-checkouts-list',
@@ -13,14 +14,24 @@ export class CheckoutsListComponent implements OnInit {
   allCheckouts: Checkout[];
   sortedCheckouts: Checkout[];
   dataSource: MatTableDataSource<Checkout>;
-  displayedColumns: string[] = ["bookTitle", "firstName", "lastName"];
+  displayedColumns: string[] = ["title", "firstName", "lastName", "checkoutDate", "dueDate"];
+  selectedCheckout: Checkout;
+  selectedIndex: number;
 
-  // sorting
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
+  // Filter
+  filterBookTitle: string = "";
+  filterName: string = "";
+  filterStatus: string = "All";
+  filteredCheckouts: Checkout[];
+  statusList: string[] = ['All','Borrowed','Late','Returned'];
+
+  // Sorting
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private checkoutService: CheckoutService,
+    private router: Router
   ) {
   }
 
@@ -37,10 +48,36 @@ export class CheckoutsListComponent implements OnInit {
     this.checkoutService.getCheckouts()
       .subscribe(data => {
         this.allCheckouts = data.content;
-        this.dataSource = new MatTableDataSource<Checkout>(this.allCheckouts);
+        this.filteredCheckouts = data.content;
+        this.dataSource = new MatTableDataSource<Checkout>(this.filteredCheckouts);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
       }, error => console.error(error));
+  }
+
+  createCheckout(): void {
+    //
+  }
+
+  applyFilter(): void {
+    this.dataSource.paginator.firstPage();
+    this.filteredCheckouts = [];
+    for(let checkout of this.allCheckouts) {
+      if(checkout.borrowedBook.title.toLowerCase().includes(this.filterBookTitle.toLowerCase()) &&
+        (checkout.borrowerFirstName.toLowerCase().includes(this.filterName.toLowerCase()) ||
+          checkout.borrowerLastName.toLowerCase().includes(this.filterName.toLowerCase())) &&
+        (this.filterStatus == "All" /*|| book.status == this.filterStatus.toUpperCase()*/)) this.filteredCheckouts.push(checkout);
+    }
+    this.dataSource = new MatTableDataSource<Checkout>(this.filteredCheckouts);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  resetFilter(): void {
+    this.filterBookTitle = "";
+    this.filterName = "";
+    this.filterStatus = "All";
+    this.applyFilter();
   }
 
   sortCheckouts(sort: Sort): void {
@@ -48,11 +85,11 @@ export class CheckoutsListComponent implements OnInit {
     this.dataSource.paginator.firstPage();
 
     if(!sort.active || sort.direction === '') {
-      this.sortedCheckouts = this.allCheckouts;
+      this.sortedCheckouts = this.filteredCheckouts;
       return;
     }
 
-    this.sortedCheckouts = this.allCheckouts.slice().sort((a, b) => {
+    this.sortedCheckouts = this.filteredCheckouts.slice().sort((a, b) => {
       const isAsc = sort.direction === 'asc';
       switch (sort.active) {
         default:
@@ -63,6 +100,10 @@ export class CheckoutsListComponent implements OnInit {
           return this.compare(a.borrowerFirstName, b.borrowerFirstName, isAsc);
         case 'lastName':
           return this.compare(a.borrowerLastName, b.borrowerLastName, isAsc);
+        case 'checkoutDate':
+          return this.compare(a.checkoutDate, b.checkoutDate, isAsc);
+        case 'dueDate':
+          return this.compare(a.dueDate, b.dueDate, isAsc);
       }
     });
   }
@@ -71,13 +112,15 @@ export class CheckoutsListComponent implements OnInit {
     return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
   }
 
-  applyFilter(event: Event): void {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-
-    if(this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+  setSelected(checkout: Checkout, index: number): void {
+    if(this.selectedCheckout && this.selectedCheckout.id == checkout.id) {
+      this.selectedCheckout = undefined;
+      this.selectedIndex = -1;
+    } else {
+      this.selectedCheckout = checkout;
+      this.selectedIndex = index;
     }
+    this.router.navigate(['checkouts', this.selectedCheckout.id])
   }
 
 }
