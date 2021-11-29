fb5df8cae330d356f8110df16037cae944cc2b83
Kristjanmn <kristjanmn@hotmail.com>
2021-11-29 03:39:19 +0200

    Updated app modules, added new CSS classes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,17 +6,18 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { BooksListComponent } from './components/books-list/books-list.component';
-import { BookDetailComponent } from './components/book-detail/book-detail.component';
+import {BookDeleteDialog, BookDetailComponent} from './components/book-detail/book-detail.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CheckoutsListComponent } from "./components/checkouts-list/checkouts-list.component";
 import { CheckoutDetailComponent } from "./components/checkout-detail/checkout-detail.component";
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     BooksListComponent,
     BookDetailComponent,
+    BookDeleteDialog,
     CheckoutsListComponent,
     CheckoutDetailComponent
   ],
@@ -26,7 +27,8 @@ import {FormsModule} from "@angular/forms";
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
