2e5e9da4bec4b97685380b9ef80b552d9d0909fe
Kristjanmn <kristjanmn@hotmail.com>
2021-12-19 13:40:13 +0200

    Added comments to repositories

--- src/main/java/com/cgi/library/repository/BookRepository.java
+++ src/main/java/com/cgi/library/repository/BookRepository.java
@@ -12,20 +12,28 @@ import java.util.UUID;
 @Repository
 public interface BookRepository extends JpaRepository<Book, UUID> {
 
+    // find all books released withing specified years
     Page<Book> findAllByYearBetween(Pageable pageable, int fromYear, int toYear);
 
+    // find all books by title, author and status released withing specified years
     Page<Book> findAllByYearBetweenAndTitleContainingAndAuthorContainingAndStatus(Pageable pageable, int fromYear, int toYear, String title, String author, BookStatus status);
 
+    // find all books by title and author released withing specified years
     Page<Book> findAllByYearBetweenAndTitleContainingAndAuthorContaining(Pageable pageable, int fromYear, int toYear, String title, String author);
 
+    // find all books by title and status released withing specified years
     Page<Book> findAllByYearBetweenAndTitleContainingAndStatus(Pageable pageable, int fromYear, int toYear, String title, BookStatus status);
 
+    // find all books by title released withing specified years
     Page<Book> findAllByYearBetweenAndTitleContaining(Pageable pageable, int fromYear, int toYear, String title);
 
+    // find all books by status released withing specified years
     Page<Book> findAllByYearBetweenAndAuthorContainingAndStatus(Pageable pageable, int fromYear, int toYear, String author, BookStatus status);
 
+    // find all books by author released withing specified years
     Page<Book> findAllByYearBetweenAndAuthorContaining(Pageable pageable, int fromYear, int toYear, String author);
 
+    // find all books by status released withing specified years
     Page<Book> findAllByYearBetweenAndStatus(Pageable pageable, int fromYear, int toYear, BookStatus status);
 
 }
--- src/main/java/com/cgi/library/repository/CheckOutRepository.java
+++ src/main/java/com/cgi/library/repository/CheckOutRepository.java
@@ -12,41 +12,53 @@ import java.util.UUID;
 @Repository
 public interface CheckOutRepository extends JpaRepository<CheckOut, UUID> {
 
+    // find checkouts by book title
     Page<CheckOut> findAllByBorrowedBook_TitleContaining(Pageable pageable, String title);
 
+    // find checkouts by book title and borrower's name
     Page<CheckOut> findAllByBorrowedBook_TitleContainingAndBorrowerFirstNameContainingOrBorrowedBook_TitleContainingAndBorrowerLastNameContaining(Pageable pageable, String title1, String name1, String title2, String name2);
 
+    // find checkouts by borrower's name
     Page<CheckOut> findALlByBorrowerFirstNameContainingOrBorrowerLastNameContaining(Pageable pageable, String name1, String name2);
 
     // Borrowed example
     // Page<CheckOut> findAllByReturnedDateNull();
 
+    // find not returned checkouts by book title
     Page<CheckOut> findAllByBorrowedBook_TitleContainingAndReturnedDateIsNull(Pageable pageable, String title);
 
+    // find not returned checkouts by book title and borrower's name
     Page<CheckOut> findAllByBorrowedBook_TitleContainingAndBorrowerFirstNameContainingOrBorrowedBook_TitleContainingAndBorrowerLastNameContainingAndReturnedDateIsNull(Pageable pageable, String title1, String name1, String title2, String name2);
 
+    // find not returned checkouts by borrower's name
     Page<CheckOut> findALlByBorrowerFirstNameContainingOrBorrowerLastNameContainingAndReturnedDateIsNull(Pageable pageable, String name1, String name2);
 
     // Late example
     //Page<CheckOut> findAllByDueDateIsBeforeAndReturnedDateIsNull(LocalDate currentDate);
 
+    // find late, not returned checkouts by book title
     Page<CheckOut> findAllByBorrowedBook_TitleContainingAndDueDateIsBeforeAndReturnedDateIsNull(Pageable pageable, String title, LocalDate currentDate);
 
+    // find late, not returned checkouts by book title and borrower's name
     Page<CheckOut> findAllByBorrowedBook_TitleContainingAndBorrowerFirstNameContainingOrBorrowedBook_TitleContainingAndBorrowerLastNameContainingAndDueDateIsBeforeAndReturnedDateIsNull(Pageable pageable, String title1, String name1, String title2, String name2, LocalDate currentDate);
 
+    // find late, not returned checkouts by borrower's name
     Page<CheckOut> findALlByBorrowerFirstNameContainingOrBorrowerLastNameContainingAndDueDateIsBeforeAndReturnedDateIsNull(Pageable pageable, String name1, String name2, LocalDate currentDate);
 
     // Returned example
     // Page<CheckOut> findAllByReturnedDateNotNull();
 
+    // find returned checkouts by book title
     Page<CheckOut> findAllByBorrowedBook_TitleContainingAndReturnedDateNotNull(Pageable pageable, String title);
 
+    // find returned checkouts by book title and borrower's name
     Page<CheckOut> findAllByBorrowedBook_TitleContainingAndBorrowerFirstNameContainingOrBorrowedBook_TitleContainingAndBorrowerLastNameContainingAndReturnedDateNotNull(Pageable pageable, String title1, String name1, String title2, String name2);
 
+    // find returned checkouts by borrower's name
     Page<CheckOut> findALlByBorrowerFirstNameContainingOrBorrowerLastNameContainingAndReturnedDateNotNull(Pageable pageable, String name1, String name2);
 
     // Only by status
-    Page<CheckOut> findAllByReturnedDateIsNull(Pageable pageable);                                            // borrowed
+    Page<CheckOut> findAllByReturnedDateIsNull(Pageable pageable);                                          // borrowed
     Page<CheckOut> findAllByDueDateBeforeAndReturnedDateIsNull(Pageable pageable, LocalDate currentDate);   // late
     Page<CheckOut> findAllByReturnedDateNotNull(Pageable pageable);                                         // returned
 
